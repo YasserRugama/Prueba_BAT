82bdbdfe0e6a6b3878caf5f8151dfde6df73cf34
YasserRugama <yrugamazelaya@gmail.com>
2024-05-07 17:38:28 -0600

    pruebaBAT

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,13 @@
+
+package holamundo;
+
+
+public class HolaMundo {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        System.out.println("Este es el comienzo en el Lenguaje de Programación Java");
+    }
+    
+}
